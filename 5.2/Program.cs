5ce1151223a246e32a4ab990ca439732af934a92
Kedrovaya-shishka <Misha12082008@icloud.com>
2025-11-28 06:55:09 +0200

    Добавьте файлы проекта.

---
+++ 5.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Лабораторная (22)
+//Базовый уровень
+Console.Write("Введите строку: ");
+string text = Console.ReadLine();
+int count1 = 0;
+int count2 = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == '!') count1++;
+    if (text[i] == '?') count2++;
+}
+if (count1 > count2)
+    Console.WriteLine("Символ '!' встречается чаще.");
+else if (count2 > count1)
+    Console.WriteLine("Символ '?' встречается чаще.");
+else
+    Console.WriteLine("Они встречаются одинаково.");
---
+++ Lab5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Лабораторная 5.1 (22)
+//Средний уровень
+Console.WriteLine("Введите 10 символов:");
+char[] mass = new char[10];
+for (int i = 0; i < 10; i++)
+{
+    mass[i] = Console.ReadKey().KeyChar;
+}
+Console.WriteLine();
+char duplicate = '\0'; // найденная буква
+for (int i = 0; i < mass.Length; i++)
+{
+    for (int j = i + 1; j < mass.Length; j++)
+    {
+        if (mass[i] == mass[j])
+        {
+            duplicate = mass[i];
+            break;
+        }
+    }
+    if (duplicate != '\0') break;
+}
+if (duplicate != '\0')
+    Console.WriteLine($"Одинаковая буква: {duplicate}");
+else
+    Console.WriteLine("Одинаковых букв нет.");
